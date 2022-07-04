8efc33d14dc06b67bfd08e52f65dac962d6e25c1
rasul <shiroeqky@gmail.com>
2022-07-05 02:27:55 +0500

    feat: bands

--- src/modules/bands/bands.resolver.ts
+++ src/modules/bands/bands.resolver.ts
@@ -1,4 +1,37 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { CreateBand, UpdateBand } from 'src/graphql'
+import { BandsService } from './bands.service'
 
 @Resolver()
-export class BandsResolver {}
+export class BandsResolver {
+  constructor(private readonly bandsService: BandsService) {}
+
+  @Mutation('createBand')
+  async createBand(@Args('band') band: CreateBand, @Context('token') token: any) {
+    return this.bandsService.create(band, token)
+  }
+
+  @Mutation('updateBand')
+  async updateBand(
+    @Args('id') id: string,
+    @Args('band') band: UpdateBand,
+    @Context('token') token: any
+  ) {
+    return this.bandsService.update(id, band, token)
+  }
+
+  @Mutation('deleteBand')
+  async deleteBand(@Args('id') id: string, @Context('token') token: any) {
+    return this.bandsService.delete(id, token)
+  }
+
+  @Query('getByIdBand')
+  async getById(@Args('id') id: string) {
+    return this.bandsService.getById(id)
+  }
+
+  @Query('getAllBands')
+  async getAll(@Args('Paginate') { limit, offset }) {
+    return this.bandsService.getAll({ limit, offset })
+  }
+}
--- src/modules/bands/bands.service.ts
+++ src/modules/bands/bands.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import axios, { AxiosInstance } from 'axios'
+import { CreateBand, Paginate, UpdateBand } from 'src/graphql'
 
 @Injectable()
-export class BandsService {}
+export class BandsService {
+  client: AxiosInstance
+
+  constructor() {
+    this.client = axios.create({ baseURL: process.env.BANDS_URL })
+  }
+
+  async getById(id: string) {
+    const { data } = await this.client.get(`/${id}`)
+    return data
+  }
+
+  async getAll({ limit, offset }: Paginate) {
+    try {
+      const { data } = await this.client.get(`?limit=${limit}&offset=${offset}`)
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async create(band: CreateBand, token: any) {
+    try {
+      const { data } = await this.client.post('/', band, {
+        headers: { Authorization: token },
+      })
+
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async update(id: string, band: UpdateBand, token: any) {
+    try {
+      const { data } = await this.client.put(`/${id}`, band, {
+        headers: { Authorization: token },
+      })
+
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async delete(id: string, token: any) {
+    try {
+      const { data } = await this.client.delete(`/${id}`, { headers: { Authorization: token } })
+      return data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
